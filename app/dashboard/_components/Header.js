113a0dfda264066c6eb7272e2a3f045becd50d68
Aamir Shahzadah <aamirshahzadah@gmail.com>
2025-01-04 08:18:26 +0500

    Header fix

--- app/dashboard/_components/Header.js
+++ app/dashboard/_components/Header.js
@@ -1,9 +1,10 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 function Header() {
   return (
-    <div>
-      
+    <div className='flex justify-end p-5 shadow-sm'>
+      <UserButton />
     </div>
   )
 }
